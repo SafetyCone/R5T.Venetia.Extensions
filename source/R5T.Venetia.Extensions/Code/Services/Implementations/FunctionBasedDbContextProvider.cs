a602744dd2e97890924d30cb20e16a282341afbe
MinexAutomation <minex.automation@gmail.com>
2019-12-27 00:00:17 -0800

    Added a function-based IDbContextProvider implementation.

---
+++ source/R5T.Venetia.Extensions/Code/Services/Implementations/FunctionBasedDbContextProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace R5T.Venetia
+{
+    public class FunctionBasedDbContextProvider<TDbContext> : IDbContextProvider<TDbContext>
+        where TDbContext : DbContext
+    {
+        private Func<DbContextOptions<TDbContext>, TDbContext> DbContextConstructor { get; }
+
+
+        public FunctionBasedDbContextProvider(Func<DbContextOptions<TDbContext>, TDbContext> dbContextConstructor)
+        {
+            this.DbContextConstructor = dbContextConstructor;
+        }
+
+        public TDbContext GetDbContext(DbContextOptions<TDbContext> dbContextOptions)
+        {
+            var dbContext = this.DbContextConstructor(dbContextOptions);
+            return dbContext;
+        }
+    }
+}
