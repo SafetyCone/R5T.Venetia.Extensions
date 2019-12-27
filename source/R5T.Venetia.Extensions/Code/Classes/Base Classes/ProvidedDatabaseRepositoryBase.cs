8d4e157aa7a7a22b135df0bd411220547ffe3c35
MinexAutomation <minex.automation@gmail.com>
2019-12-27 00:00:54 -0800

    Created a database repository base class that uses an IDbContextProvider.

---
+++ source/R5T.Venetia.Extensions/Code/Classes/Base Classes/ProvidedDatabaseRepositoryBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace R5T.Venetia
+{
+    /// <summary>
+    /// Base class for repositories that can create their own database context instance from DI-container provided <see cref="DbContextOptions{TContext}"/>.
+    /// </summary>
+    public abstract class ProvidedDatabaseRepositoryBase<TDbContext>
+        where TDbContext : DbContext
+    {
+        protected DbContextOptions<TDbContext> DbContextOptions { get; }
+        protected IDbContextProvider<TDbContext> DbContextProvider { get; }
+
+
+        public ProvidedDatabaseRepositoryBase(DbContextOptions<TDbContext> dbContextOptions, IDbContextProvider<TDbContext> dbContextProvider)
+        {
+            this.DbContextOptions = dbContextOptions;
+            this.DbContextProvider = dbContextProvider;
+        }
+
+        /// <summary>
+        /// Basically just a wrapper for callings the <see cref="IDbContextProvider{TDbContext}"/>.
+        /// </summary>
+        public TDbContext GetNewDbContext()
+        {
+            var dbContext = this.DbContextProvider.GetDbContext(this.DbContextOptions);
+            return dbContext;
+        }
+
+        protected void ExecuteInContext(Action<TDbContext> action)
+        {
+            using (var dbContext = this.GetNewDbContext())
+            {
+                action(dbContext);
+            }
+        }
+
+        protected TOutput ExecuteInContext<TOutput>(Func<TDbContext, TOutput> function)
+        {
+            using (var dbContext = this.GetNewDbContext())
+            {
+                var output = function(dbContext);
+                return output;
+            }
+        }
+    }
+}
