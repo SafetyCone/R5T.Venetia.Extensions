72b7e2ca4e8ccce64ba97c0d5bee3ab9915f4378
Tasha Arvanitis <tasha.arvanitis@gmail.com>
2020-08-20 10:54:06 -0700

    Add a type restriction to WasFound

--- source/R5T.Venetia.Extensions/Code/Helpers/EntityHelper.cs
+++ source/R5T.Venetia.Extensions/Code/Helpers/EntityHelper.cs
@@ -5,7 +5,7 @@ namespace R5T.Venetia
 {
     public static class EntityHelper
     {
-        public static bool WasFound<T>(T entity)
+        public static bool WasFound<T>(T entity) where T: class
         {
             var wasFound = entity == default;
             return wasFound;
