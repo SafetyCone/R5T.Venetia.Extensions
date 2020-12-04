c2376d927fe8f7ec27b1fd966a3e44347a11006f
David Coats <david@getsafetycone.com>
2020-12-03 22:23:51 -0800

    Adding missing DbContext extensions

---
+++ source/R5T.Venetia.Extensions/Code/Extensions/DbContextExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace R5T.Venetia
+{
+    public static class DbContextExtensions
+    {
+        public static async Task ModifyEntitySingle<TDbContext, TEntity>(this TDbContext dbContext,
+            Func<TDbContext, IQueryable<TEntity>> entitySelector,
+            Expression<Func<TEntity, bool>> predicate,
+            Action<TEntity> entityModifer)
+            where TDbContext: DbContext
+        {
+            var entities = entitySelector(dbContext);
+
+            var entity = await entities.Where(predicate).SingleAsync();
+
+            entityModifer(entity);
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
