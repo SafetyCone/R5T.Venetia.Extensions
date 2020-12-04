9beb959a520088cafc8f21f7de2f39dc046a3036
David Coats <david@getsafetycone.com>
2020-12-03 22:23:39 -0800

    Added an app-type to entity-type key equality checker, and a DbContext extension to modify a single entity

--- source/R5T.Venetia.Extensions/Code/Helpers/EntityHelper.cs
+++ source/R5T.Venetia.Extensions/Code/Helpers/EntityHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 
 namespace R5T.Venetia
@@ -9,6 +10,28 @@ namespace R5T.Venetia
         {
             var wasFound = entity != default;
             return wasFound;
+        }
+
+        public static bool CheckKeyEquality<TAppType, TEntityType, TKey>(
+            TAppType appType, Func<TAppType, TKey> appTypeKeySelector,
+            TEntityType entityType, Func<TEntityType, TKey> entityTypeKeySelector,
+            IEqualityComparer<TKey> keyEqualityComparer)
+        {
+            var appTypeKey = appTypeKeySelector(appType);
+            var entityTypeKey = entityTypeKeySelector(entityType);
+
+            var keysEqual = keyEqualityComparer.Equals(appTypeKey, entityTypeKey);
+            return keysEqual;
+        }
+
+        public static bool CheckKeyEquality<TAppType, TEntityType, TKey>(
+            TAppType appType, Func<TAppType, TKey> appTypeKeySelector,
+            TEntityType entityType, Func<TEntityType, TKey> entityTypeKeySelector)
+        {
+            var equalityComparer = EqualityComparer<TKey>.Default;
+
+            var keysEqual = EntityHelper.CheckKeyEquality(appType, appTypeKeySelector, entityType, entityTypeKeySelector);
+            return keysEqual;
         }
     }
 }
